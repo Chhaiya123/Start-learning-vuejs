e70bc37f461fa7c58764c6794521cd8846eb0ca0
Chhaiya123 <yoeunchhaiya1@gmail.com>
2025-07-08 17:15:08 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Title1 : {{ text1 }}</h1>
+  <h1>Title2 : {{ text2 }}</h1>
+</template>
+
+<script setup>
+  import { ref } from 'vue';
+  // non-reactive
+  const text1 = 'Hello World!!'
+  // reactive
+  const text2 = ref('Hello World!!')
+</script>
